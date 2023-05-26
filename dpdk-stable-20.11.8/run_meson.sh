5776ddf4464317cbf32171ba320b37af7181f824
cerotyki <cerotyki@gmail.com>
2023-05-26 16:38:07 +0000

    removed

--- dpdk-stable-20.11.8/run_meson.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-meson -Dbuild.c_args=-I/homes/borispi/rdma-core/build/include -Dbuild.c_link_args=-L/homes/borispi/rdma-core/build/lib -Dbuild.pkg_config_path=/homes/borispi/rdma-core/build/lib/pkgconfig -Dexamples=l2fwd,l3fwd build
